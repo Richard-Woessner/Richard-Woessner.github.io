43f241853e847a525ff4e86017867750ed20b44a
Richard Woessner <71599819+Richard-Woessner@users.noreply.github.com>
2021-06-04 16:57:20 -0400

    Added first card

---
+++ port/src/comp/Pages/Interests.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from "react";
+import { AppBar, Toolbar, Box, Paper } from "@material-ui/core";
+import { sizing, border } from "@material-ui/system";
+
+
+class Interests extends React.Component {
+  render() {
+    return (
+        <Box height="95vh" border={1} id="about">
+          <Paper>sdf</Paper>
+        </Box>
+    );
+  }
+}
+export default Interests;
