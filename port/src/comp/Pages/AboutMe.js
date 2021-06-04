732f64842a5550c3f834d6dc00a65ee6a79fa88c
Richard-Woessner <Woessnrw@mail.uc.edu>
2021-06-04 13:01:00 -0400

    Created Nav bar and added scrolling

---
+++ port/src/comp/Pages/AboutMe.js
@@ -0,0 +1,23 @@
+import React, { Fragment } from "react";
+import { AppBar, Toolbar, Box, Paper } from "@material-ui/core";
+import { sizing, border } from "@material-ui/system";
+
+
+class Aboutme extends React.Component {
+  render() {
+    return (
+      <div>
+        <Box height="95vh" border={1} id="about">
+          <Paper>sdf</Paper>
+        </Box>
+        <Box height="95vh" border={1} id="work">
+          <Paper>sdf</Paper>
+        </Box>
+        <Box height="95vh" border={1} id="interests">
+          <Paper>sdf</Paper>
+        </Box>
+      </div>
+    );
+  }
+}
+export default Aboutme;
